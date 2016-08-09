d95266753f2fbe4a65b803b3b6c9dbb238ce60c4
IvanSostarko <ivan.sostarko@hotmail.com>
2016-08-09 23:52:09 +0200

    Initial

---
+++ app/pages/item/item.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+    templateUrl: 'build/pages/item/item.html'
+})
+export class ItemPage {
+    constructor(private navCtrl: NavController) {
+
+    }
+}
